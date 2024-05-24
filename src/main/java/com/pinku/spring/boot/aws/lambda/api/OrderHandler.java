b7ddcbea1c53fb1549e9f2068a739bdecdf2c352
Pinku <kpinku32@gmail.com>
2024-05-24 19:22:47 +0530

    First commit

---
+++ src/main/java/com/pinku/spring/boot/aws/lambda/api/OrderHandler.java
@@ -0,0 +1,7 @@
+package com.pinku.spring.boot.aws.lambda.api;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+public class OrderHandler extends SpringBootRequestHandler<String, Object> {
+
+}
